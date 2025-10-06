3bc28948fb8ccb083870d6ae262f088ba81cacb2
Jose A. Vergara <jvergara@uv.mx>
2025-10-06 09:06:43 -0600

    primer commit

---
+++ programa1_if-else.cpp
@@ -0,0 +1,9 @@
+// Elabore programas que muestre un menú de productos y retorne el precio del 
+// producto seleccionado: Programa 1: Utilice if-else
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ programa2_switch.cpp
@@ -0,0 +1,10 @@
+// Elabore programas que muestre un menú de productos y retorne el precio del 
+// producto seleccionado: Programa 2: Utilice switch
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
