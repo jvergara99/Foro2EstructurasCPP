48ab2e5dd6bd4c35280eb8a2b2f2f84483f8f968
Jose A. Vergara <jvergara@uv.mx>
2025-10-06 09:20:52 -0600

    Programa 1 con if-else

--- programa1_if-else.cpp
+++ programa1_if-else.cpp
@@ -4,6 +4,25 @@
 using namespace std;
 
 int main() {
+    int opcion;
+    do {
+
+        cout << "====Menú====" << endl;
+        cout << "1) Tortas" << endl;
+        cout << "2) Palomitas" << endl;
+        cout << "3) Agua" << endl;
+        cout << "4) Salir" << endl;
+        cout << "Opción: ";
+        cin >> opcion;
+
+        if (opcion == 1) {
+            cout << "Tortas: $45" << endl;
+        } else if (opcion == 2) {
+            cout << "Palomitas: $10" << endl;
+        } else if (opcion == 3) {
+            cout << "Aguas 600ml: $15" << endl;
+        } 
+    } while (opcion != 4);
 
     return 0;
 }
