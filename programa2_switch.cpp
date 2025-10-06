18b5312474e451fe0470366f6992460bab79de4d
Jose A. Vergara <jvergara@uv.mx>
2025-10-06 09:28:18 -0600

    Programa 2 con switch

--- programa2_switch.cpp
+++ programa2_switch.cpp
@@ -5,6 +5,32 @@
 using namespace std;
 
 int main() {
+int opcion;
+    do {
+        cout << "====Menú====" << endl;
+        cout << "1) Tortas" << endl;
+        cout << "2) Palomitas" << endl;
+        cout << "3) Agua" << endl;
+        cout << "4) Salir" << endl;
+        cout << "Opción: ";
+        cin >> opcion;
 
+        switch (opcion)
+        {
+        case 1:
+            cout << "Tortas: $45" << endl;
+            break;
+        case 2:
+            cout << "Palomitas: $10" << endl;
+            break;
+        case 3:
+            cout << "Aguas: $15" << endl;
+            break;
+        default:
+            cout << "Opción inválida, selecciona una correcta" << endl;
+            break;
+        } 
+
+    } while (opcion != 4);
     return 0;
 }
